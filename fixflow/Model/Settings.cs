08c31dd35617598801db18100f90f2b61ec028aa
Tristan McRaven <tristanmcraven@gmail.com>
2024-12-11 16:23:43 +0500

    dump

--- fixflow/Model/Settings.cs
+++ fixflow/Model/Settings.cs
@@ -11,5 +11,32 @@ namespace fixflow.Model
         public bool Something { get; set; }
         public bool EnableAutoUpdates { get; set; }
         public bool CheckForUpdates { get; set; }
+        public bool RememberWindowSize { get; set; }
+        public bool RememberWindowLocation { get; set; }
+        public Dictionary<string, WindowSize> WindowSizes { get; set; }
+        public Dictionary<string, WindowLocation> WindowLocations { get; set; }
+    }
+
+    public struct WindowSize
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public WindowSize(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+
+    public struct WindowLocation
+    {
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public WindowLocation(double top, double left)
+        {
+            Top = top;
+            Left = left;
+        }
     }
 }
